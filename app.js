42e9f29bdb35874954c7ca263f1eff4daccd04d7
jannatflowers <jannatflowers4@gmail.com>
2021-01-27 00:09:17 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// login button form
+const firstlogin = document.getElementById("login_submit");
+firstlogin .addEventListener("click",function(){
+    // console.log("logiin button click")
+    document.getElementById("login_area").style.display="none";
+    document.getElementById("transiton_area").style.display="block";
+})
+// transition area start
+const secondlogin = document.getElementById("deposit_btn");
