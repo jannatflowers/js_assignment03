6f50aa5174dafeca114ec2ce59f6b1a73a6868cd
jannatflowers <jannatflowers4@gmail.com>
2021-01-28 00:46:58 +0600

    update file

--- app.js
+++ app.js
@@ -6,4 +6,31 @@ firstlogin .addEventListener("click",function(){
     document.getElementById("transiton_area").style.display="block";
 })
 // transition area start
-const secondlogin = document.getElementById("deposit_btn");
+// const secondlogin = document.getElementById("deposit_btn");
+// secondlogin.addEventListener("click",function(){
+//     const depositeinpu =parseFloa(document.getElementById("deposit_inpu").value);
+
+// const  currentdepo  = parseFloat(document.getElementById("curent_deposite").innerHTML);
+// const totalbalance =parseFloat(document.getElementById("total_amount").innerHTML);
+// console.log(totalbalance);
+// })
+const secondsub = document.getElementById("deposit_btn");
+secondsub.addEventListener("click", function(){
+    const depositinput = parseFloat(document.getElementById("deposit_inpu").value);
+    const currentdepo = parseFloat(document.getElementById("curent_deposite").innerHTML);
+    const totaldepo = parseFloat(document.getElementById("total_amount").innerHTML);
+   
+    document.getElementById("curent_deposite").innerHTML=currentdepo+depositinput;
+    document.getElementById("total_amount").innerHTML=totaldepo+depositinput;
+    document.getElementById("deposit_inpu").value="";
+
+})
+const thirdsubmit = document.getElementById("withdrow_btn");
+thirdsubmit.addEventListener("click", function(){
+    const widthinput =parseFloat(document.getElementById("withdraw_input").value);
+    const widthbalance =parseFloat(document.getElementById("withdraw").innerHTML);
+    const totalbalance = parseFloat(document.getElementById("total_amount").innerHTML);
+    document.getElementById("withdraw").innerHTML=widthbalance+widthinput;
+    document.getElementById("total_amount").innerHTML=totalbalance-widthinput;
+    document.getElementById("withdraw_input").value="";
+})
